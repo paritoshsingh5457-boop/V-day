605d0d4008a7fd5817426326413000c6fd6429d7
paritoshsingh5457-boop <paritoshsingh5457@gmail.com>
2026-02-14 14:22:11 +0530

    Add main interaction logic and date/time display

---
+++ script.js
@@ -0,0 +1,11 @@
+// main interaction logic
+console.log('Welcome to V-day!');
+
+// Function to display current date and time
+function displayCurrentDateTime() {
+    const now = new Date();
+    console.log(`Current Date and Time: ${now.toUTCString()}`);
+}
+
+// Call the function
+displayCurrentDateTime();
